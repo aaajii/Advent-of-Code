28e37dbe856c63233f5e367df3dce11b88dbe6d6
Jimuel William P. Banawan <jwpbanawan@addu.edu.ph>
2020-12-03 08:00:19 +0000

    day 1 part 2 brodies! (A LOOOT MORE SLOWER SOLUTION... but it works???)

--- day1.js
+++ day1.js
@@ -2,17 +2,22 @@ const fs = require("fs");
 const text = fs.readFileSync("./input.txt")+'';
 const input = text.split("\n")
 
-let a = b = input
+let a = b = c = input
 
 let pair = [];
 
+
+//this solution takes a lot of time man :(((
 a.forEach(i => {
   b.forEach(j=> {
-    i = parseInt(i)
-    j = parseInt(j)
-    if (i+j==2020) {
-      pair = [i,j]
-    }
+    c.forEach(k=>{
+        i = parseInt(i)
+        j = parseInt(j)
+        k = parseInt(k)
+        if (i+j+k==2020) {
+          pair = [i,j,k]
+        }
+    })
   })
 })
 
